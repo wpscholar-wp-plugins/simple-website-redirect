eb9b1cb97d88de27506cd2d8e81f6926d4a57922
Micah Wood <micah@wpscholar.com>
2018-02-04 16:19:01 -0500

    Initial commit

---
+++ SimpleWebsiteRedirect.php
@@ -0,0 +1,266 @@
+<?php
+
+/*
+ * Plugin Name: Simple Website Redirect
+ * Plugin URI:
+ * Description: A simple plugin designed to redirect an entire website (except the WordPress admin) to another website.
+ * Version:     1.0
+ * Author:      Micah Wood
+ * Author URI:  https://wpscholar.com
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: simple-website-redirect
+ */
+
+/**
+ * Class SimpleWebsiteRedirect
+ */
+class SimpleWebsiteRedirect {
+
+	/**
+	 * @var string Plugin admin menu page slug.
+	 */
+	const PAGE = 'simple-website-redirect';
+
+	/**
+	 * Hook our custom functions into WordPress core.
+	 */
+	public static function initialize() {
+
+		load_plugin_textdomain( 'simple-website-redirect', false, __DIR__ . '/languages' );
+
+		add_action( 'init', array( __CLASS__, '_init' ) );
+		add_action( 'admin_init', array( __CLASS__, '_admin_init' ) );
+		add_action( 'admin_menu', array( __CLASS__, '_admin_menu' ), 99 );
+		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( __CLASS__, '_plugin_action_links' ) );
+	}
+
+	/**
+	 * Primary functionality - handles website redirect based on current configuration.
+	 */
+	public static function _init() {
+		if ( ! is_admin() ) {
+			$redirect_enabled = wp_validate_boolean( get_option( 'simple_website_redirect_status', false ) );
+			$redirect_url = self::sanitize_redirect_url( get_option( 'simple_website_redirect_url' ) );
+			if ( $redirect_enabled && ! empty( $redirect_url ) ) {
+				$redirect_type = self::sanitize_redirect_type( get_option( 'simple_website_redirect_type' ) );
+				wp_redirect( $redirect_url . $_SERVER['REQUEST_URI'], $redirect_type );
+				exit;
+			}
+		}
+	}
+
+	/**
+	 * Sanitize redirect URL
+	 *
+	 * @param string $url
+	 *
+	 * @return string
+	 */
+	public static function sanitize_redirect_url( $url ) {
+		$clean_url = '';
+		$scheme = parse_url( $url, PHP_URL_SCHEME );
+		$host = untrailingslashit( parse_url( $url, PHP_URL_HOST ) );
+		if ( $scheme && $host ) {
+			$clean_url = "$scheme://$host";
+		}
+
+		return $clean_url;
+	}
+
+	/**
+	 * Sanitize redirect type
+	 *
+	 * @param int $type
+	 *
+	 * @return int
+	 */
+	public static function sanitize_redirect_type( $type ) {
+		$clean_type = 301;
+		if ( 302 === absint( $type ) ) {
+			$clean_type = 302;
+		}
+
+		return $clean_type;
+	}
+
+	/**
+	 * Register our settings.
+	 */
+	public static function _admin_init() {
+
+		register_setting( self::PAGE, 'simple_website_redirect_url', array( __CLASS__, 'sanitize_redirect_url' ) );
+		register_setting( self::PAGE, 'simple_website_redirect_type', array( __CLASS__, 'sanitize_redirect_type' ) );
+		register_setting( self::PAGE, 'simple_website_redirect_status', 'wp_validate_boolean' );
+
+		add_settings_section(
+			'settings',
+			esc_html__( 'Settings', 'simple-website-redirect' ),
+			'__return_null',
+			self::PAGE
+		);
+
+		add_settings_field(
+			'simple_website_redirect_url',
+			esc_html__( 'Redirect URL', 'simple-website-redirect' ),
+			array( __CLASS__, '_input_field' ),
+			self::PAGE,
+			'settings',
+			[
+				'name'        => 'simple_website_redirect_url',
+				'type'        => 'url',
+				'class'       => 'regular-text',
+				'placeholder' => 'https://',
+			]
+		);
+
+		add_settings_field(
+			'simple_website_redirect_type',
+			esc_html__( 'Redirect Type', 'simple-website-redirect' ),
+			array( __CLASS__, '_select_field' ),
+			self::PAGE,
+			'settings',
+			[
+				'name'    => 'simple_website_redirect_type',
+				'options' => array(
+					301 => esc_html__( 'Permanent', 'simple-website-redirect' ),
+					302 => esc_html__( 'Temporary', 'simple-website-redirect' ),
+				),
+			]
+		);
+
+		add_settings_field(
+			'simple_website_redirect_status',
+			esc_html__( 'Redirect Status', 'simple-website-redirect' ),
+			array( __CLASS__, '_select_field' ),
+			self::PAGE,
+			'settings',
+			[
+				'name'    => 'simple_website_redirect_status',
+				'options' => array(
+					0 => esc_html__( 'Disabled', 'simple-website-redirect' ),
+					1 => esc_html__( 'Enabled', 'simple-website-redirect' ),
+				),
+			]
+		);
+
+	}
+
+	/**
+	 * Add our custom admin menu page.
+	 */
+	public static function _admin_menu() {
+
+		add_submenu_page(
+			'options-general.php',
+			esc_html__( 'Simple Website Redirect', 'simple-website-redirect' ),
+			esc_html__( 'Website Redirect', 'simple-website-redirect' ),
+			'manage_options',
+			self::PAGE,
+			array( __CLASS__, '_render_page' )
+		);
+	}
+
+	/**
+	 * Render admin page.
+	 */
+	public static function _render_page() {
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		if ( isset( $_GET['settings-updated'] ) ) {
+			add_settings_error(
+				self::PAGE,
+				'simple_website_redirect_settings_updated',
+				esc_html__( 'Settings Updated', 'simple-website-redirect' ),
+				'updated'
+			);
+		}
+
+		settings_errors( self::PAGE );
+		?>
+        <div class="wrap">
+            <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+            <form action="options.php" method="post">
+				<?php
+				do_settings_sections( self::PAGE );
+				settings_fields( self::PAGE );
+				submit_button( esc_html__( 'Save Settings', 'simple-website-redirect' ) );
+				?>
+            </form>
+        </div>
+		<?php
+	}
+
+	/**
+	 * Add settings link to plugin on plugin list in the WP admin.
+	 *
+	 * @param array $links
+	 *
+	 * @return array
+	 */
+	public static function _plugin_action_links( $links ) {
+
+		$settings_link = sprintf(
+			'<a href="%s">%s</a>',
+			esc_url( admin_url( 'options-general.php?page=' . self::PAGE ) ),
+			esc_html__( 'Settings', 'simple-website-redirect' )
+		);
+		$links[] = $settings_link;
+
+		return $links;
+	}
+
+	/**
+	 * Outputs an input field.
+	 *
+	 * @param array $args
+	 */
+	public static function _input_field( array $args ) {
+
+		$name = isset( $args['name'] ) ? $args['name'] : '';
+
+		printf(
+			'<input type="%s" class="%s" name="%s" value="%s" placeholder="%s" />',
+			esc_attr( isset( $args['type'] ) ? $args['type'] : 'text' ),
+			esc_attr( isset( $args['class'] ) ? $args['class'] : '' ),
+			esc_attr( $name ),
+			esc_attr( get_option( $name, '' ) ),
+			esc_attr( isset( $args['placeholder'] ) ? $args['placeholder'] : '' )
+
+		);
+	}
+
+	/**
+	 * Outputs a select field.
+	 *
+	 * @param array $args
+	 */
+	public static function _select_field( array $args ) {
+
+		$name = isset( $args['name'] ) ? $args['name'] : '';
+		$options = isset( $args['options'] ) ? (array) $args['options'] : [];
+		$value = get_option( $name, '' );
+
+		$opening_tag = sprintf( '<select name="%s">', esc_attr( $name ) ) . PHP_EOL;
+
+		$option_elements = array();
+		foreach ( $options as $option_value => $option_label ) {
+			$option_elements[] = sprintf(
+				'<option value="%s"%s>%s</option>',
+				esc_attr( $option_value ),
+				selected( $option_value, $value, false ),
+				esc_html( $option_label )
+			);
+		}
+
+		$closing_tag = '</select>' . PHP_EOL;
+
+		echo $opening_tag . implode( PHP_EOL, $option_elements ) . $closing_tag;
+	}
+
+}
+
+SimpleWebsiteRedirect::initialize();
---
+++ readme.txt
@@ -0,0 +1,63 @@
+=== Simple Website Redirect ===
+Contributors: woodent
+Donate link: https://www.paypal.me/wpscholar
+Tags: site, website, redirect, redirection, forwarding
+Requires at least: 4.0
+Tested up to: 4.9.2
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+A simple plugin designed to redirect an entire website (except the WordPress admin) to another website.
+
+== Description ==
+
+The **Simple Website Redirect** plugin allows to you redirect an entire website (except the WordPress admin) to another website.
+
+The URL path and query string is preserved when redirecting to the new site. Ideally, the new site would handle any one-off redirects where a URL for an old page should point to a new page. The [Redirection](https://wordpress.org/plugins/redirection/) plugin is great for this purpose.
+
+Find out more about [website redirects for SEO](https://moz.com/learn/seo/redirection).
+
+= Usage Instructions =
+
+Using this plugin is simple:
+
+1. Install the plugin
+2. Activate the plugin
+3. Go to 'Settings' in the WordPress admin menu and then click on 'Website Redirect'.
+4. Enter the URL you want to redirect the site to, set the desired redirection type, set the status to 'Enabled' and save your changes!
+
+Note: Redirection type can either be 'Temporary' or 'Permanent'. It is recommended that you start with 'Temporary' while testing and then convert to 'Permanent' after testing for maximum SEO benefit. Please be aware that [browsers cache permanent redirects](https://wpscholar.com/blog/browser-caching-301-redirects/).
+
+== Installation ==
+
+= Prerequisites =
+If you don't meet the below requirements, I highly recommend you upgrade your WordPress install or move to a web host that supports a more recent version of PHP.
+
+* Requires WordPress version 4.0 or greater
+* Requires PHP version 5.2.4 or greater ( PHP version 5.2.4 is required to run WordPress version 4.0 )
+
+= The Easy Way =
+
+1. In your WordPress admin, go to 'Plugins' and then click on 'Add New'.
+2. In the search box, type in 'Simple Website Redirect' and hit enter.  This plugin should be the first and likely the only result.
+3. Click on the 'Install' link.
+4. Once installed, click the 'Activate this plugin' link.
+
+= The Hard Way =
+
+1. Download the .zip file containing the plugin.
+2. Upload the file into your `/wp-content/plugins/` directory and unzip
+3. Find the plugin in the WordPress admin on the 'Plugins' page and click 'Activate'
+
+== Screenshots ==
+
+1. Just enter the URL you want to redirect the site to, set the desired redirection type, set the status to 'Enabled' and save!
+
+== Changelog ==
+
+= 1.0 =
+* Initial commit
+
+== Upgrade Notice ==
+
