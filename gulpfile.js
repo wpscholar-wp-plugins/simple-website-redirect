e51aa9b2033d3fda582a1a930fb6d541c10cd333
Micah Wood <micah@wpscholar.com>
2019-06-26 11:45:05 -0400

    Updated Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,51 @@
-let gulp = require('gulp');
-let shell = require('gulp-shell');
-let del = require('del');
+'use strict';
+
+let gulp = require( 'gulp' );
+let shell = require( 'shelljs' );
+let del = require( 'del' );
 
 let config = {
-    svn: {
-        url: 'http://plugins.svn.wordpress.org/simple-website-redirect/',
-        src: [
-            './**',
-            '!**/screenshot-*.png',
-            '!**/screenshot-*.jpg',
-            '!**/screenshot-*.gif',
-            '!**/icon-*.png',
-            '!**/svn',
-            '!**/svn/**',
-            '!**/readme.md',
-            '!**/package.json',
-            '!**/package-lock.json',
-            '!**/node_modules',
-            '!**/node_modules/**',
-            '!**/bower.json',
-            '!**/bower_components',
-            '!**/bower_components/**',
-            '!**/gulpfile.js',
-            '!**/composer.*',
-            '!**/vendor',
-            '!**/vendor/**',
-        ],
-        dest: './svn/trunk',
-        clean: './svn/trunk/**/*'
-    }
+	svn: {
+		url: 'http://plugins.svn.wordpress.org/simple-website-redirect/',
+		src: [
+			'./**',
+			'!**/screenshot-*.png',
+			'!**/screenshot-*.jpg',
+			'!**/screenshot-*.gif',
+			'!**/icon-*.png',
+			'!**/svn',
+			'!**/svn/**',
+			'!**/readme.md',
+			'!**/package.json',
+			'!**/package-lock.json',
+			'!**/node_modules',
+			'!**/node_modules/**',
+			'!**/bower.json',
+			'!**/bower_components',
+			'!**/bower_components/**',
+			'!**/gulpfile.js',
+			'!**/composer.*',
+			'!**/vendor',
+			'!**/vendor/**',
+		],
+		dest: './svn/trunk',
+		clean: './svn/trunk/**/*'
+	}
 };
 
-gulp.task('svn:checkout', shell.task('svn co ' + config.svn.url + ' svn'));
+gulp.task( 'checkout', (done) => {
+	shell.exec( 'svn co ' + config.svn.url + ' svn' );
+	done();
+} );
 
-gulp.task('svn:clean', function () {
-    return del(config.svn.clean);
-});
+gulp.task( 'clean', () => {
+	return del( config.svn.clean );
+} );
 
-gulp.task('svn:copy', ['svn:clean'], function () {
-    return gulp.src(config.svn.src).pipe(gulp.dest(config.svn.dest));
-});
+gulp.task( 'copy', () => {
+	return gulp.src( config.svn.src ).pipe( gulp.dest( config.svn.dest ) );
+} );
 
-gulp.task('svn:stage', ['svn:copy']);
+gulp.task( 'stage', gulp.series( 'clean', 'copy' ) );
+
+gulp.task( 'default', gulp.series( 'stage' ) );
